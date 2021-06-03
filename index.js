e8cd17a4e7fc5b36f54c873e64e43b26f87e0b8e
Peter Phan <peterphan0422@gmail.com>
2021-06-03 16:28:01 -0500

    feat: cow

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { router } = require("./routes/dinoRoutes");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
@@ -19,6 +21,21 @@ mongoose
 
 const db = mongoose.connection;
 
+app.get("/", (req, res) => {
+	res.status(200).send(`
+    <h1>Hello from Memeit API ^.^</h1>
+    <pre>
+            ______
+          &lt; Moo! &gt;
+            ------
+                  \\   ^__^
+                    \\ (oo)\\_______
+                      (__)\\       )\\/\\
+                          ||----w |
+                          ||     ||
+    </pre>
+  `);
+});
 app.listen(PORT, () => {
 	console.log(`Server is now listening to port ${PORT}`);
 });
